86b9e130f1adbba658f372e2da08f0473b28f498
Roni <ronisalman@email.arizona.edu>
2020-04-16 23:27:53 -0700

    mobile menu dropdown addition

--- zoo.js
+++ zoo.js
@@ -1,2 +1,29 @@
 // Created by Roni Salman
 
+function init(){
+	menuClick();
+}
+
+function menuClick(){
+	let active=false;
+	const menu=document.querySelector("#menu");
+	const layer=document.querySelectorAll(".layer");
+	menu.addEventListener("click",function(){
+		if(active===false){
+			for(let i=0;i<layer.length;i++){
+				layer[i].classList.remove("menuUnclicked");
+				layer[i].classList.add("menuClicked");
+			}
+			active=true;
+		}
+		else{
+			for(let i=0;i<layer.length;i++){
+				layer[i].classList.add("menuUnclicked");
+			}
+			active=false;
+		}
+
+	});	
+}
+
+init();
